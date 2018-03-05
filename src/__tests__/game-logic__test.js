164ad8de0bbb3975aff0a727c240320cab280611
Guillaume Bouffard <guillaume@getkahoot.com>
2018-03-05 18:39:59 +0000

    game logic knows when the result of a game is a draw

--- src/__tests__/game-logic__test.js
+++ src/__tests__/game-logic__test.js
@@ -1,10 +1,14 @@
 import gameLogic from '../services/game-logic';
 
 describe('game logic', () => {
-  xit('chooses a random hand for the CPU when playing a 1 player game', () => {
+  it('chooses a random hand for the CPU when playing a 1 player game', () => {
+    expect(gameLogic.choseCPUHand())
+      .toEqual('Rock' || 'Paper' || 'Scissors');
   });
 
-  xit('knows when the result of a game is a draw', () => {
+  it('knows when the result of a game is a draw', () => {
+    expect(gameLogic.gameResult('Rock', 'Rock'))
+      .toEqual(`It's a Draw!`);
   });
 
   xit('knows when the player won the game', () => {
--- src/services/game-logic.js
+++ src/services/game-logic.js
@@ -1,4 +1,15 @@
+import _ from 'lodash';
+
 const gameLogic = {
+  choseCPUHand() {
+    return _.sample(['Rock', 'Paper', 'Scissors']); 
+  },
+
+  gameResult(firstHand, secondHand) {
+    if (firstHand === secondHand) {
+      return `It's a Draw!`
+    }
+  }
 };
 
 export default gameLogic;

9c57beec5088170c605cbfe8d7cbbfb3a1f1eff5
Guillaume Bouffard <guillaume@getkahoot.com>
2018-03-05 18:10:03 +0000

    Write and skip game-logic unit test

---
+++ src/__tests__/game-logic__test.js
@@ -0,0 +1,15 @@
+import gameLogic from '../services/game-logic';
+
+describe('game logic', () => {
+  xit('chooses a random hand for the CPU when playing a 1 player game', () => {
+  });
+
+  xit('knows when the result of a game is a draw', () => {
+  });
+
+  xit('knows when the player won the game', () => {
+  });
+
+  xit('knows when the player lost the game', () => {
+  });
+});
---
+++ src/services/game-logic.js
@@ -0,0 +1,4 @@
+const gameLogic = {
+};
+
+export default gameLogic;

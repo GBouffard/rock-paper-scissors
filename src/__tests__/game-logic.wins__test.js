1d4916a664f2eaefbf2ed42857a59cec8f374f18
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2018-10-01 15:58:42 +0100

    Add empty tests for game-logic.wins

---
+++ src/__tests__/game-logic.wins__test.js
@@ -0,0 +1,18 @@
+import gameLogic from '../services/game-logic';
+
+describe('gameLogic.wins', () => {
+  xit('starts with a value of 0 for draws, player1 and player2', () => {
+  });
+
+  xit('adds 1 to the wins.draws value when the result is a draw', () => {
+  });
+
+  xit('adds 1 to the wins.player1 value when the player1 wins the game', () => {
+  });
+
+  xit('adds 1 to the wins.player2 value when the player2 wins the game', () => {
+  });
+
+  xit('knows the correct values of wins for draws, player1 and player 2 whem multiple games have been played', () => {
+  });
+});
